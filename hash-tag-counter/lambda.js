0019244712795449ee422ce003c12937bdc199e0
Udith <uditharosha@gmail.com>
2020-03-30 15:24:39 +0530

    Changed code to use async-await syntax

--- hash-tag-counter/lambda.js
+++ hash-tag-counter/lambda.js
@@ -1,53 +1,53 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function (event, context, callback) {
+exports.handler = async (event) => {
 
 	console.log("Received hash tag message event", event);
-	
-	event.Records.forEach(function (record) {
+
+	for (const record of event.Records) {
+
 		let payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
 		let payloadObj = JSON.parse(payload);
 		console.log('Decoded payload successfully :', payload);
 
 		let application = payloadObj.Application;
 		let hashTags = payloadObj.HashTags;
-		console.log('Decoded Application : ' + application + ' and hashtags : ' + hashTags);
-		
-		hashTags.forEach(hashTag => {
-			let currentCount = 0;
-			ddb.get({
-				TableName: 'HashTags',
-				Key: { 'Application': application, 'HashTag': hashTag },
-				AttributesToGet: [
-					'Count'
-				]
-			}, function (err, data) {
-				if (err) {
-					console.log('Error while getting the existing count for the hashtag : ' + hashTag + ' and application : ' + 
-						applciation, err);
-				} else {
-					console.log('Successfully fetched the existing row for hashtag : ' +  hashTag + ' in application : ' + 
-						application, data);
-					let currentCount = data.Item ? data.Item.Count + 1 : 1;
+		console.log('Decoded Application :', application, 'and hashtags :', hashTags);
 
-					ddb.put({
+		for (const hashTag of hashTags) {
+			let currentCount = 0;
+			try {
+				let data = await ddb.get({
+					TableName: 'HashTags',
+					Key: { 'Application': application, 'HashTag': hashTag },
+					AttributesToGet: [
+						'Count'
+					]
+				}).promise();
+
+				console.log('Successfully fetched the existing row for hashtag :', hashTag, 'in application :', application, data);
+				let currentCount = data.Item ? data.Item.Count + 1 : 1;
+
+				try {
+					let data = await ddb.put({
 						TableName: 'HashTags',
 						Item: { 'Application': application, 'HashTag': hashTag, 'Count': currentCount }
-					}, function (err, data) {
-						if (err) {
-							console.log('Error while updating the hashcount for hashtag : ' + hashTag +' and application : ' + 
-							applciation, err);
-						} else {
-							console.log('Successfully updated the hashtag count to : ' + currentCount + ' for hashtag : ' + 
-								hashTag + ' in application : ' + application, data);
-						}
-					});
+					}).promise();
+					
+					console.log('Successfully updated the hashtag count to :', currentCount, 'for hashtag :', hashTag, 'in application :',
+						application, data);
+
+				} catch (err) {
+					console.log('Error while updating the hashcount for hashtag :', hashTag, 'and application :', application, err);
 				}
-			});
-		})
+			} catch (err) {
+				console.log('Error while getting the existing count for the hashtag : ' + hashTag + ' and application : ' +
+					applciation, err);
+			}
+		}
 
-	});
+	}
 
-	callback(null, 'Successfully executed');
-}
+	return 'Successfully executed';
+};
--- hash-tag-counter/push-test-data.js
+++ hash-tag-counter/push-test-data.js
@@ -1,51 +1,50 @@
 let AWS = require('aws-sdk');
 const kinesis = new AWS.Kinesis();
 
-exports.handler = function (event, context, callback) {
+exports.handler = async (event) => {
 
 	console.log("Receiveed messages for hash tags", event.body);
 
 	let hashTagMessages = JSON.parse(event.body).HashTagData;
-	hashTagMessages.forEach(hashTagMessage => {
+
+	let putRecordResponses = [];
+	let successCount = 0;
+	let failedCount = 0;
+
+	for (const hashTagMessage of hashTagMessages) {
+
 		let message = JSON.stringify(hashTagMessage);
 		console.log("HashTag message", message)
-		kinesis.putRecord({
-			Data: message,
-			PartitionKey: 'SocialMedia',
-			StreamName: 'hash-tag-stream'
-		}).promise()
-			.then(putRecordData => {
-				console.log("Push message successfully to hash-tag-stream with response", putRecordData);
-				let response = {
-					'statusCode': 200,
-					'headers': {
-						'Access-Control-Allow-Origin': '*',
-						'Content-Type': 'application/json'
-					},
-					'body': JSON.stringify({
-						'Code': 'Success',
-						'Message': 'Hash tag records are successfully populated to hash-tag-stream',
-						'Data': putRecordData
-					}),
-					'isBase64Encoded': false
-				};
-				callback(null, response);
-			})
-			.catch(err => {
-				console.log("Error while pushing message to hash-tag-stream with response", err);
-				let response = {
-					'statusCode': err.statusCode,
-					'headers': {
-						'Access-Control-Allow-Origin': '*',
-						'Content-Type': 'application/json'
-					},
-					'body': JSON.stringify({
-						'Code': err.code,
-						'Message': err.message
-					}),
-					'isBase64Encoded': false
-				};
-				callback(null, response);
-			});
-	})
-}
+
+		try {
+			let putRecordData = await kinesis.putRecord({
+				Data: message,
+				PartitionKey: 'SocialMedia',
+				StreamName: 'hash-tag-stream'
+			}).promise();
+
+			console.log("Push message successfully to hash-tag-stream with response", putRecordData);
+			putRecordResponses.push(putRecordData);
+			successCount++;
+
+		} catch (err) {
+			console.log("Error while pushing message to hash-tag-stream with response", err);
+			failedCount++;
+		}
+	}
+
+	let response = {
+		'statusCode': 200,
+		'headers': {
+			'Access-Control-Allow-Origin': '*',
+			'Content-Type': 'application/json'
+		},
+		'body': JSON.stringify({
+			'Success Count': successCount,
+			'Failed Count': failedCount,
+			'Success Data': putRecordResponses
+		}),
+		'isBase64Encoded': false
+	};
+	return response;
+};
